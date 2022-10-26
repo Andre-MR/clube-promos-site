e663532e6c2c9336d49543b29f010273d3a9cf28
Andre-MR <andreinternet@gmail.com>
2022-10-26 02:11:49 -0300

    test disable images

--- components/card/card-header/card-header.tsx
+++ components/card/card-header/card-header.tsx
@@ -5,7 +5,7 @@ import Offer from "../../../models/offer";
 export default function CardHeader({ offer }: { offer: Offer }) {
   return (
     <div className="relative flex justify-center">
-      <CardImage offer={offer} />
+      {/* <CardImage offer={offer} /> */}
       <CardImageBackground />
     </div>
   );
