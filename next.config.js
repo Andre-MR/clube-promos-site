296d24f0580c9c38a0d7f0054ca7d98e604fdfb8
Andre-MR <andreinternet@gmail.com>
2022-10-28 00:13:53 -0300

    env and names changes

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ["clubepromos.s3.sa-east-1.amazonaws.com"],
+    domains: [process.env.AWS_S3_DOMAIN],
   },
 };
 
